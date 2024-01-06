4c46294de8c3750de70e775692aadca3395eb2f0
guillermosifu <sifuentes1201@gmail.com>
2024-01-05 22:58:31 -0500

    cnahge

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,34 +1,44 @@
-import{useState,useEffect} from 'react'
-import{useParams,useNavigate} from "react-router-dom"
-import{Container,Grid,Button,Chip} from "@mui/material"
-import { getDataPokemon } from '../../helpers';
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { Container, Grid, Button, Chip } from "@mui/material";
+import { getDataPokemon } from "../../helpers";
 
-const Detail = ()=>{
-  const{name} = useParams();
+const Detail = () => {
+  const { name } = useParams();
   const history = useNavigate();
 
+  const [country, setCountry] = useState({});
 
-  const[country,setCountry]=useState({});
-  
-  const FetchCountry= async()=>{
-    const response = await getDataPokemon(`https://restcountries.com/v3.1/name/${name}`)
-  }
+  const FetchCountry = async () => {
+    const response = await getDataPokemon(
+      `https://restcountries.com/v3.1/name/${name}`
+    );
+   
+  };
 
-  useEffect(()=>{
-  FetchCountry()
-  },[])
+  useEffect(() => {
+    FetchCountry();
+  }, []);
 
-
-
-    return(
-  <Container>
-    <Button variant='outlined' onClick={()=>history(-1)}>
+  return (
+    <Container>
+      <Button variant="outlined" onClick={() => history(-1)}>
         Volver
-    </Button>
-  </Container>
+      </Button>
+      {Object.keys(country).length > 0 && (
+        <Grid container spacing={3}>
+          <Grid item md={6}>
+          <img src={country.flags.svg} width={400} alt="" />
+          </Grid>
+          <Grid item md={6}>
+            <h3>{country.name.common}</h3>
+            <p>capital:{country.capital}</p>
 
-    )
-}
-
+          </Grid>
+        </Grid>
+      )}
+    </Container>
+  );
+};
 
 export default Detail;
